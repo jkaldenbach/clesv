e8b12068a87f558aa369da4e601440638cbf4740
Justin Kaldenbach <kaldejm@gmail.com>
2020-01-19 13:57:02 -0500

    add line breaks to Daily Light passage render

--- src/ui.js
+++ src/ui.js
@@ -1,12 +1,15 @@
+const currentWeekNumber  = require('current-week-number')
+
 const api = require('./esv-api')
 const blessed = require('blessed')
 const daily = require('../assets/passages.json')
+const ncc = require('../assets/ncc.json')
 
 const screen = blessed.screen({
   smartCSR: true
 })
 
-screen.title = 'CLISV'
+screen.title = 'CLESV'
 
 const layout = blessed.layout({
   parent: screen,
@@ -33,10 +36,11 @@ const emptyview = blessed.box({
   width: '50%',
   tags: true,
   content: `{center}
-Welcome to CLISV
+Welcome to CLESV
 
 P: Passage Lookup
 D: Daily Light on the Daily Path
+C: New City Catechism
 Q: Quit
 {/}`,
   style: { align: 'center' }
@@ -63,7 +67,7 @@ const help = blessed.text({
   top: '100%-2',
   left: 'center',
   width: '100%',
-  content: 'P: Passage  D: Daily  Q: Quit'
+  content: 'P: Passage  D: Daily  C: New City Catechism  Q: Quit'
 })
 
 const form = blessed.form({
@@ -103,15 +107,25 @@ screen.key('p', (ch, key) => {
   input.focus()
 })
 screen.key('d', async (ch, key) => {
+  renderPassage('Loading...')
   const n = new Date()
   const m = n.getMonth() + 1
   const d = n.getDate()
   const mResp = await api.text(daily[m][d].morning)
   const eResp = await api.text(daily[m][d].evening)
-  const mText = mResp.data.passages.reduce((t, p) => `${t}${p}`, '')
-  const eText = eResp.data.passages.reduce((t, p) => `${t}${p}`, '')
+  const mText = mResp.data.passages.reduce((t, p) => `${t}${p}\n\n`, '')
+  const eText = eResp.data.passages.reduce((t, p) => `${t}${p}\n\n`, '')
   const text = `MORNING\n${mText}\n\n\nEVENING\n${eText}`
   renderPassage(text)
+})
+screen.key('c', async (ch, key) => {
+  const wk = currentWeekNumber()
+  const parts = ncc.questions[wk]
+  const question = `Question:\n${parts[0]}\n\nAnswer:\n${parts[1]}`;
+  renderPassage(question)
+  const pResp = await api.text(parts[2])
+  const p = pResp.data.passages.reduce((t, p) => `${t}${p}`, '')
+  renderPassage(`${question}\n\n${p}`)
 })
 
 screen.render()
